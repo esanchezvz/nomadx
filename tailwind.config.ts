bfad3b05ce9f588200b4801be147a421fd0b5a5b
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-09-23 13:50:30 -0600

    feat: initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "brand-primary": "var(--brand-primary)",
+        "brand-secondary": "var(--brand-secondary)",
       },
     },
   },
